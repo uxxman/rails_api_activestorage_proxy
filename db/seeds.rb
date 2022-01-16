a6711ef549428871dead55024df46d7e3b117c0f
Usman <uxman.sherwani@gmail.com>
2022-01-16 14:17:11 +0500

    Adding seed data and image

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+post = Post.create! title: 'Post One'
+post.image.attach(io: File.open(Rails.root.join('ss.png')), filename: 'image.png')
